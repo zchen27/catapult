f0e4c1aeb7cadd7ced3d75d14149415695b00175
Zhehao Chen <zchen27@gmail.com>
2013-02-19 22:47:13 -0500

    Adding Additional Definitions

--- Project1/catapult.h
+++ Project1/catapult.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <math.h>;
+#include <math.h>
 
 class catapult
 {
@@ -8,6 +8,10 @@ public:
 	catapult(void);
 	~catapult(void);
 	#define G 9.8
+	#define CD 0.47
+	#define RHO 1.293
+	#define E 2.71828
+	#define VERY_SMALL_NUMBER pow(1.0,-8.0)
 	#ifndef M_PI
 	#define PI atan(1.0)*4
 	#else
