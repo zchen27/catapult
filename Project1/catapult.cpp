cdb87009110e6e059e1f53a773ae115577656174
Zhehao Chen <zchen27@gmail.com>
2013-03-25 08:35:18 -0400

    Fine. All things worked out.
    Ballistic Table is legit, very legit.

--- Project1/catapult.cpp
+++ Project1/catapult.cpp
@@ -4,9 +4,10 @@
 #include <sstream>
 #include <fstream>
 
-double m;
-double r;
-
+double m = 0.1; //kg
+double r = 0.015; //m
+//double c = 0.5 * RHO * CD * PI * pow(r, 2);
+double c = VERY_SMALL_NUMBER;
 catapult::catapult(void)
 {
 }
@@ -18,8 +19,15 @@ catapult::~catapult(void)
 double work_spring(double theta)
 {
 	double W;
-	//W = fnInt(torque, theta, 0, thea)
-	W = (3.78025 * pow(theta, 4)) + (16.751 * pow(theta, 3)) + (16.5485 * (theta, 2)) + (0.7801 * theta) /*Indefinite integral at 0 is 0*/;
+	//W = fnInt(torque, theta, 0, theta)
+	W = (11.2803 * pow(theta, 4)) - (16.751 * pow(theta, 3)) + (16.5485 * pow(theta, 2)) + (0.7801 * theta);
+	return W;
+}
+
+double work_gravity(double theta)
+{
+	double W;
+	W = -1 * ((sin(theta) * LENGTH_BAR / 2 * MASS_BAR * G) + (sin(theta) * (LENGTH_BAR - r) * G));
 	return W;
 }
 
@@ -55,16 +63,27 @@ double flight_distance(double v0, double theta, double t)
 *	Now with air resistance (unfinished)
 */
 
-double flight_time_ar(double v0, double theta, double h, double c)
+double flight_time_ar(double v0, double theta, double h)
 {
 	double v0y = v0 * sin(theta);
-	return sqrt(m / (c * G)) * (atan(sqrt(c / (m*G)) * v0y) - acos(1 / (exp(c * h /m) * sqrt(1 + c / (m*G) * pow(v0y,2)))));
+	double temp1 = sqrt(m / (c * G));//
+	double temp2 = sqrt(c / (m * G)) * v0y;
+	double temp3 = atan(temp2);//
+	double temp4 = c * h / m;
+	double temp5 = 1 * pow(E, temp4);
+	double temp6 = 1 + (c / (m * G) * pow(v0y, 2));
+	double temp7 = 1 / (temp5 * sqrt(temp6));
+	double temp8 = acos(temp7);//
+	//double t = sqrt(m / (c * G)) * (atan(sqrt(c / (m*G)) * v0y) - acos(1 / (pow(E, (c * h /m)) * sqrt(1 + (c / (m*G) * pow(v0y,2))))));
+	double t = temp1 * (temp3 - temp8);
+	return t;
 }
 
-double flight_distance_ar(double v0, double theta, double h, double c, double t)
+double flight_distance_ar(double v0, double theta, double t)
 {
 	double v0x = v0 * cos(theta);
-	return m / c * log(1 + v0x * c * t / m);
+	double x =  m / c * log(1 + v0x * c * t / m);
+	return x;
 }
 
 
@@ -95,7 +114,7 @@ double velocity(double usp, double i)
  */
 double rotational_inertia(double len, double offset, double rod_mass, double proj_mass)
 {
-    double i_bar = rod_mass*pow(len,2)/3;
+    double i_bar = rod_mass*pow((len/2),2)/12 + rod_mass*pow(offset,2); //Not exactly on the edge
     double i_proj = proj_mass*pow(len,2);
     return i_bar + i_proj;
 }
@@ -163,38 +182,50 @@ int main(int argc, const char * argv[])
 	double v0;
 	double t;
 	double x;
-	while (thetad <= 40)
+	double alpha = radians(36.0);
+	double bench = 0.6096;
+
+	ofstream results;
+	results.open("results.txt");
+	results << "angel: \t";
+	results << "x: "<< endl;
+	while (thetad <= 60)
 	{
-		thetar = radians(thetad);
-		U = work_spring(thetar);
-		I = rotational_inertia(0.25, 0.02, 0.1, 0.021);
-		omega = velocity(U, I);
-		v0 = omega * 0.25;
-		t = flight_time(v0, 0.61575, sin(0.61575) * 0.25 + 0.6096);
-		x = flight_distance(v0, 0.61575, t);
 		
+		thetar = radians(thetad);
+		U = work_spring(thetar) - work_gravity(thetar);
+		I = rotational_inertia(LENGTH_BAR, (LENGTH_BAR /  2 - 0.02), m, (LENGTH_BAR));
+		omega = velocity(U, I);
+		v0 = omega * (0.25); //offset from edge
+		t = flight_time(v0, alpha, (sin(alpha) * LENGTH_BAR + bench));
+		x = flight_distance(v0, alpha, t);
+		cout << "thetad: " << thetad << endl;
+		results << thetad << "\t";
 		cout << "thetar: " << thetar << endl;
-		cout << "U: "<< U << endl;
+		cout << "U: " << U << endl;
+		cout << "I: " << I << endl;
 		cout << "omega: " << omega << endl;
 		cout << "v0: " << v0 << endl;
 		cout << "t: " << t<< endl;
 		cout << "x: " << x << endl;
+		results << x << endl;
 		cout << "" << endl;
 		thetad++;
 	}
 
 	
-	ofstream results;
-	results.open("results.txt");
-	results << "test\n";
-	
-	 double i = 0;
-	while(i <= 45)
-	{
-		results << work_spring(i);
-		results << "\t";
-		i += 0.5;
-	}
+
+	/*results.open("results.txt");
+	results << "test"<< endl;
+	results << "thetad: /t"<<;
+		results << "thetar: /t" <<;
+		results << "U: /t/t" <<;
+		results << "I: /t/t" << I << endl;
+		results << "omega: /t" << omega << endl;
+		results << "v0: /t/t" << v0 << endl;
+		results << "t: /t/t" << t<< endl;
+		results << "x: /t/t" << x << endl;
+		results << "" << endl;*/
 
 	//insert while loop that cycles through all possible angles by increments of 1 degree.
 	results.close();
--- Project1/catapult.h
+++ Project1/catapult.h
@@ -7,10 +7,12 @@ class catapult
 public:
 	catapult(void);
 	~catapult(void);
-	#define G 9.8
+	#define G 9.8  // m/(s^2)
 	#define CD 0.47
-	#define RHO 1.293
+	#define RHO 1.293 // kg/(m^3)
 	#define E 2.71828
+	#define MASS_BAR 0.1
+	#define LENGTH_BAR 0.25
 	#define VERY_SMALL_NUMBER pow(1.0,-8.0)
 	#ifndef M_PI
 	#define PI atan(1.0)*4
